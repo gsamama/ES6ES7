897dc8869d733900a0f9ee708d7aba965184c71a
Gilbert Samama <gsamama@gmail.com>
2019-10-05 21:58:07 +0300

    webpack done

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,16 @@
-const numbers = [1, 2, 3, 4, 5, 8];
+import ImageMessage from './model/image-message.model';
+
+const m = new ImageMessage();
+console.log('not empty');
+console.dir(m);
+
+/*const numbers = [1, 2, 3, 4, 5, 8];
 /*
 console.log(numbers.filter(n => n > 4));
 console.log(numbers.reduce((acum, current) => acum+current, 10)) // arrSum + 10
 
 console.log(numbers.find(n => n === 3));
-*/
+
 //const [a, b, c, d, e, f=0] = numbers;
 //const [a, b, ...c] = numbers;
 let [a, b, , , , c] = numbers;
@@ -19,4 +25,4 @@ console.log(otherNumbers)
 
 const s = new Set();
 s.add(1).add(2).add(3).add(2);
-console.log(s);
+console.log(s);*/
