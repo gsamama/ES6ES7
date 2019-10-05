9edc6e215a902c8ee0fbfde985e3f725cab547c5
Gilbert Samama <gsamama@gmail.com>
2019-10-05 19:18:10 +0300

    scoped

---
+++ app/index.js
@@ -0,0 +1,22 @@
+const numbers = [1, 2, 3, 4, 5, 8];
+/*
+console.log(numbers.filter(n => n > 4));
+console.log(numbers.reduce((acum, current) => acum+current, 10)) // arrSum + 10
+
+console.log(numbers.find(n => n === 3));
+*/
+//const [a, b, c, d, e, f=0] = numbers;
+//const [a, b, ...c] = numbers;
+let [a, b, , , , c] = numbers;
+
+console.log(a, b, c);
+
+[b, a] = [a, b];
+console.log(a, b)
+
+const otherNumbers = [10 , 20 , 30 , ...numbers, 30];
+console.log(otherNumbers)
+
+const s = new Set();
+s.add(1).add(2).add(3).add(2);
+console.log(s);
